cdbca6fb1a2d370d7bfec93b30b069b1711a62f3
Nick Lee <leexnick@gmail.com>
2019-06-19 12:29:03 -0700

    finish search ui

--- src/Components/SearchUI.js
+++ src/Components/SearchUI.js
@@ -3,15 +3,107 @@ import './SearchUI.css'
 
 export default class SearchUI extends Component {
   data = [
-    89, 30, 25, 32, 72, 70, 51, 42, 25, 24, 
-    53, 55, 78, 50, 13, 40, 48, 32, 26, 2, 14, 
-    33, 45, 72, 56, 44, 21, 88, 27, 68, 15, 62, 
-    93, 98, 73, 28, 16, 46, 87, 28, 65, 38, 67, 
-    16, 85, 63, 23, 69, 64, 91, 9, 70, 81, 27, 
-    97, 82, 6, 88, 3, 7, 46, 13, 11, 64, 76, 31, 
-    26, 38, 28, 13, 17, 69, 90, 1, 6, 7, 64, 43, 
-    9, 73, 80, 98, 46, 27, 22, 87, 49, 83, 6, 39, 
-    42, 51, 54, 84, 34, 53, 78, 40, 14, 5]
+    89,
+    30,
+    25,
+    32,
+    72,
+    70,
+    51,
+    42,
+    25,
+    24,
+    53,
+    55,
+    78,
+    50,
+    13,
+    40,
+    48,
+    32,
+    26,
+    2,
+    14,
+    33,
+    45,
+    72,
+    56,
+    44,
+    21,
+    88,
+    27,
+    68,
+    15,
+    62,
+    93,
+    98,
+    73,
+    28,
+    16,
+    46,
+    87,
+    28,
+    65,
+    38,
+    67,
+    16,
+    85,
+    63,
+    23,
+    69,
+    64,
+    91,
+    9,
+    70,
+    81,
+    27,
+    97,
+    82,
+    6,
+    88,
+    3,
+    7,
+    46,
+    13,
+    11,
+    64,
+    76,
+    31,
+    26,
+    38,
+    28,
+    13,
+    17,
+    69,
+    90,
+    1,
+    6,
+    7,
+    64,
+    43,
+    9,
+    73,
+    80,
+    98,
+    46,
+    27,
+    22,
+    87,
+    49,
+    83,
+    6,
+    39,
+    42,
+    51,
+    54,
+    84,
+    34,
+    53,
+    78,
+    40,
+    14,
+    5
+  ]
 
   state = {
     input: null,
@@ -19,15 +111,51 @@ export default class SearchUI extends Component {
     count: 0
   }
 
-  handleChange = (e) => {
-    this.setState({ input: e.target.value})
+  handleChange = e => {
+    this.setState({ input: e.target.value, search: null })
   }
 
-  handleBinSearch = () => {}
-
-  handleLinSearch = (e) => {
+  handleBinSearch = e => {
     e.preventDefault()
-    
+
+    let count = 0
+    let sortedData = [...this.data]
+    sortedData.sort()
+
+    count = this.binarySearch(sortedData, this.state.input)
+    console.log(count)
+    this.setState({
+      search: 'Binary Search',
+      count: count
+    })
+  }
+
+  binarySearch(array, value, start, end, currCount) {
+    start = start === undefined ? 0 : start
+    end = end === undefined ? array.length : end
+    currCount = currCount === undefined ? 0 : currCount
+
+    if (start > end) {
+      return -1
+    }
+
+    const index = Math.floor((start + end) / 2)
+    const item = array[index]
+    currCount++
+
+    console.log(start, end)
+    if (Number(item) === Number(value)) {
+      return currCount
+    } else if (item < value) {
+      return this.binarySearch(array, value, index + 1, end, currCount)
+    } else if (item > value) {
+      return this.binarySearch(array, value, start, index - 1, currCount)
+    }
+  }
+
+  handleLinSearch = e => {
+    e.preventDefault()
+
     let count = 0
     let found = false
     const value = Number(this.state.input)
@@ -49,13 +177,18 @@ export default class SearchUI extends Component {
   }
 
   renderResult() {
+    console.log(this.state)
     if (!this.state.search) {
-      console.log(this.state)
       return 'Click a button to perform search'
     }
 
-    if (this.state.count < 0) {
-      return `${this.state.input} NOT FOUND...`
+    if (this.state.count < 0 || this.state.count === undefined) {
+      return (
+        <>
+          <p>{this.state.search} Performed</p>
+          <p>{this.state.input} NOT FOUND...</p>
+        </>
+      )
     }
 
     return (
@@ -67,14 +200,13 @@ export default class SearchUI extends Component {
   }
 
   render() {
-    
     return (
       <div className="search-wrapper">
         <form>
           <input
             type="text"
             className="input"
-            name='input'
+            name="input"
             placeholder="enter num to search for"
             onChange={this.handleChange}
           />
